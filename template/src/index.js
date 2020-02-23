7effeeba399da90b6991432259cb41c8f587e778
Fabiano Martins <fabiano.paula.martins@gmail.com>
2020-02-23 17:31:05 -0300

    Fixing lint errors and add babel config file

--- template/src/index.js
+++ template/src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import './styles/bootstrap/bootstrap.scss';
 
